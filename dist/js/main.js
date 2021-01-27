00eed35dc5b30d82eaab7739090feb3775e40015
canosaer <canosaer@gmail.com>
2021-01-26 23:58:47 -0500

    compressed and lazy

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,2 @@
-"use strict";
-
-console.log("Hello World from main.js! \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
+document.addEventListener("DOMContentLoaded",(function(){var e;if("IntersectionObserver"in window){e=document.querySelectorAll(".lazy");var n=new IntersectionObserver((function(e,t){e.forEach((function(e){if(e.isIntersecting){var t=e.target;t.classList.remove("lazy"),n.unobserve(t)}}))}));e.forEach((function(e){n.observe(e)}))}else{var t;function o(){t&&clearTimeout(t),t=setTimeout((function(){var n=window.pageYOffset;e.forEach((function(e){e.offsetTop<window.innerHeight+n&&(e.src=e.dataset.src,e.classList.remove("lazy"))})),0==e.length&&(document.removeEventListener("scroll",o),window.removeEventListener("resize",o),window.removeEventListener("orientationChange",o))}),20)}e=document.querySelectorAll(".lazy"),document.addEventListener("scroll",o),window.addEventListener("resize",o),window.addEventListener("orientationChange",o)}}));
 //# sourceMappingURL=main.js.map
